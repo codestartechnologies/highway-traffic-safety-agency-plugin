86191d54656b2f3a4c993800466d37d911e48af2
Chijindu G. Nzeako <chijindunzeako517@gmail.com>
2023-02-11 19:23:37 +0100

    updated html view for shortcodes
    - updated contact form and newsletter form shortcode

--- views/public/shortcodes/contact-form.php
+++ views/public/shortcodes/contact-form.php
@@ -1,25 +1,19 @@
-<!-- Contact Form ShortCode -->
-<div class="ui section divider"></div>
-<h4 class="text-uppercase mb-3"> <?php esc_html_e( 'Contact us and we will reply you as soon as possible', 'htsa-plugin' ); ?> </h4>
-<div class="col-lg-6 mx-auto-">
-    <form action="" method="post" class="ui form" data-htsa-id="contactForm">
-        <div class="required field">
-            <label for="htsaContactFormName"> <?php esc_html_e( 'Name', 'htsa-plugin' ); ?> </label>
-            <input type="text" name="name" id="htsaContactFormName" class="border-secondary rounded-4" />
-        </div>
-        <div class="required field">
-            <label for="htsaContactFormEmail"> <?php esc_html_e( 'Email', 'htsa-plugin' ); ?> </label>
-            <input type="text" name="email" id="htsaContactFormEmail" class="border-secondary rounded-4" />
-        </div>
-        <div class="required field">
-            <label for="htsaContactFormMessage"> <?php esc_html_e( 'Message', 'htsa-plugin' ); ?> </label>
-            <textarea name="message" id="htsaContactFormMessage" class="border-secondary rounded-5"></textarea>
-        </div>
-        <button type="submit" class="ui black button"> <?php esc_html_e( 'Submit', 'htsa-plugin' ); ?> </button>
-        <button type="button" class="ui black labeled icon button" style="display: none;">
-            <i class="loading spinner icon"></i> <?php esc_html_e( 'Loading', 'htsa-plugin' ) ?>
-        </button>
-        <div class="ui error message"></div>
-    </form>
-</div>
-<!-- End Contact Form ShortCode -->
+<form action="" method="post" class="ui form" data-htsa-id="contactForm">
+    <div class="required field">
+        <label for="htsaContactFormName"> <?php esc_html_e( 'Name', 'htsa-plugin' ); ?> </label>
+        <input type="text" name="name" id="htsaContactFormName" class="border-secondary rounded-4" />
+    </div>
+    <div class="required field">
+        <label for="htsaContactFormEmail"> <?php esc_html_e( 'Email', 'htsa-plugin' ); ?> </label>
+        <input type="text" name="email" id="htsaContactFormEmail" class="border-secondary rounded-4" />
+    </div>
+    <div class="required field">
+        <label for="htsaContactFormMessage"> <?php esc_html_e( 'Message', 'htsa-plugin' ); ?> </label>
+        <textarea name="message" id="htsaContactFormMessage" class="border-secondary rounded-5"></textarea>
+    </div>
+    <button type="submit" class="ui black button"> <?php esc_html_e( 'Submit', 'htsa-plugin' ); ?> </button>
+    <button type="button" class="ui black labeled icon button" style="display: none;">
+        <i class="loading spinner icon"></i> <?php esc_html_e( 'Loading', 'htsa-plugin' ) ?>
+    </button>
+    <div class="ui error message"></div>
+</form>
--- views/public/shortcodes/newsletter-form.php
+++ views/public/shortcodes/newsletter-form.php
@@ -1,14 +1,15 @@
-<h5 class="fw-normal text-capitalize htsa-text-yellow mb-2"> <?php esc_html_e( 'Newsletter form', 'htsa-plugin' ); ?> </h5>
-<form action="#" class="ui inverted form" data-htsa-id="newsletterForm" onsubmit="return false;">
-    <div class="required field">
-        <label for=""> <?php esc_html_e( 'Fullname', 'htsa-plugin' ); ?> </label>
-        <input type="text" name="name" id="htsaNewsletterFormName" class="rounded-0" />
-    </div>
-    <div class="required field">
-        <label for=""> <?php esc_html_e( 'Email Address', 'htsa-plugin' ); ?> </label>
-        <input type="email" name="email" id="htsaNewsletterFormEmail" class="rounded-0" />
-    </div>
-    <button class="ui button bg-warning text-dark rounded-0" type="submit">
-        <?php esc_html_e( 'Subscribe me to newsletters', 'htsa-plugin' ); ?>
-    </button>
-</form>
+<div class="htsa-bg-dark p-2">
+    <form action="#" class="ui inverted form" data-htsa-id="newsletterForm" onsubmit="return false;">
+        <div class="required field">
+            <label for=""> <?php esc_html_e( 'Fullname', 'htsa-plugin' ); ?> </label>
+            <input type="text" name="name" id="htsaNewsletterFormName" class="rounded-0" />
+        </div>
+        <div class="required field">
+            <label for=""> <?php esc_html_e( 'Email Address', 'htsa-plugin' ); ?> </label>
+            <input type="email" name="email" id="htsaNewsletterFormEmail" class="rounded-0" />
+        </div>
+        <button class="ui button bg-warning text-dark rounded-0" type="submit">
+            <?php esc_html_e( 'Subscribe me to newsletters', 'htsa-plugin' ); ?>
+        </button>
+    </form>
+</div>
